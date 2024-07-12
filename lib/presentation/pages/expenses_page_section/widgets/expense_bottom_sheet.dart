475f29480c73c5bcbcabd5aa26450eabf78817c4
Jaswanth Harikrishna <jaswanthkriz@gmail.com>
2024-07-12 22:45:13 +0530

    added some changes

--- lib/presentation/pages/expenses_page_section/widgets/expense_bottom_sheet.dart
+++ lib/presentation/pages/expenses_page_section/widgets/expense_bottom_sheet.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:udemy_expense_tracker/model/expense_model.dart';
 
 class ExpenseBottomSheet extends StatefulWidget {
   const ExpenseBottomSheet({super.key});
@@ -8,22 +9,30 @@ class ExpenseBottomSheet extends StatefulWidget {
 }
 
 class _ExpenseBottomSheetState extends State<ExpenseBottomSheet> {
-  // var _enteredInput = " ";
-  // void _saveTitleInput(String inputValue) {
-  //   _enteredInput = inputValue;
-  // }
-
   final TextEditingController _titleController = TextEditingController();
   final _amountController = TextEditingController();
+  DateTime? _selectedDate;
+  Catogory _selectedCatogory = Catogory.leisure;
 
-  final now = DateTime.now();
-  final firstDate = DateTime(DateTime.now().year -1,DateTime.now().month,DateTime.now().day);
-  _datePicker() {
-    showDatePicker(
+  _presentDatePicker() async {
+    final now = DateTime.now();
+    final firstDate = DateTime(now.year - 1, now.month, now.day);
+    final pickedDate = await showDatePicker(
       context: context,
-      firstDate: now,
+      initialDate: now,
+      firstDate: firstDate,
       lastDate: now,
     );
+    setState(() {
+      _selectedDate = pickedDate;
+    });
+  }
+
+  void _submitExpenseDataForm() {
+    final enterdAmount = double.parse(_amountController.text);
+    if(_titleController.text.trim().isEmpty){
+
+    }
   }
 
   @override
@@ -67,26 +76,52 @@ class _ExpenseBottomSheetState extends State<ExpenseBottomSheet> {
                   mainAxisAlignment: MainAxisAlignment.end,
                   //   crossAxisAlignment: CrossAxisAlignment.center,
                   children: [
-                    const Text("Select Date"),
+                    Text(
+                      _selectedDate == null
+                          ? "Select Date"
+                          : formatter.format(_selectedDate!),
+                    ),
                     IconButton(
-                        onPressed: _datePicker,
+                        onPressed: _presentDatePicker,
                         icon: const Icon(Icons.calendar_month_outlined))
                   ],
                 ),
               )
             ],
           ),
+          const SizedBox(
+            height: 20,
+          ),
           Row(
             children: [
+              DropdownButton(
+                value: _selectedCatogory,
+                items: Catogory.values
+                    .map(
+                      (catogory) => DropdownMenuItem(
+                        value: catogory,
+                        child: Text(
+                          catogory.name.toUpperCase(),
+                        ),
+                      ),
+                    )
+                    .toList(),
+                onChanged: (value) {
+                  if (value == null) {
+                    return;
+                  }
+                  setState(() {
+                    _selectedCatogory = value;
+                  });
+                },
+              ),
+              const Spacer(),
               TextButton(
                 onPressed: () => Navigator.pop(context),
                 child: const Text("Cancel"),
               ),
               ElevatedButton(
-                  onPressed: () {
-                    print(_titleController.text);
-                    print(_amountController.text);
-                  },
+                  onPressed: _submitExpenseDataForm,
                   child: const Text("Save Expense"))
             ],
           )
